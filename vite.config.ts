6da8d67acad349ab31af209f8f1b882222519f38
simonlhopkins <simonlhopkins@gmail.com>
2025-03-30 11:01:00 -0700

    dumb

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
